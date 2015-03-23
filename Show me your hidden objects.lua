fbdc023d6f2beadd193835b54c21cf60325200b2
SunnyTiTi <phamqduy@gmail.com>
2015-03-23 19:50:14 +0700

    Auto Update

--- Show me your hidden objects.lua
+++ Show me your hidden objects.lua
@@ -1,3 +1,36 @@
+local version = "0.2"
+local AUTO_UPDATE = true
+local UPDATE_SCRIPT_NAME = "Show me your hidden objects"
+local UPDATE_HOST = "raw.github.com"
+local UPDATE_PATH = "/SunnyTiTi/BoL/master/Show%20me%20your%20hidden%20objects.lua"
+local UPDATE_FILE_PATH = SCRIPT_PATH .. "Show me your hidden objects.lua"
+local UPDATE_URL = "https://" .. UPDATE_HOST .. UPDATE_PATH
+
+local ServerData
+if AUTO_UPDATE then
+	GetAsyncWebResult(UPDATE_HOST, UPDATE_PATH, function(d) ServerData = d end)
+	function update()
+		if ServerData ~= nil then
+			local ServerVersion
+			local send, tmp, sstart = nil, string.find(ServerData, "local version = \"")
+			if sstart then
+				send, tmp = string.find(ServerData, "\"", sstart+1)
+			end
+			if send then
+				ServerVersion = tonumber(string.sub(ServerData, sstart+1, send-1))
+			end
+
+			if ServerVersion ~= nil and tonumber(ServerVersion) ~= nil and tonumber(ServerVersion) > tonumber(version) then
+				DownloadFile(UPDATE_URL.."?nocache"..myHero.charName..os.clock(), UPDATE_FILE_PATH, function () print("<font color=\"#FF0000\"><b>"..UPDATE_SCRIPT_NAME..":</b> successfully updated. Reload (double F9) Please. ("..version.." => "..ServerVersion..")</font>") end)     
+			elseif ServerVersion then
+				print("<font color=\"#FF0000\"><b>"..UPDATE_SCRIPT_NAME..":</b> You have got the latest version: <u><b>"..ServerVersion.."</b></u></font>")
+			end		
+			ServerData = nil
+		end
+	end
+	AddTickCallback(update)
+end
+
 local wards = {}
 
 function OnLoad()
